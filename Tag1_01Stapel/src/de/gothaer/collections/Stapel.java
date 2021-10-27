d3c136fe098f206f09b3a9d2ed80c5385e160243
Jo Wagner <jowagner@limago.de>
2021-10-27 11:34:16 +0200

    Initial commit

--- Tag1_01Stapel/src/de/gothaer/collections/Stapel.java
+++ Tag1_01Stapel/src/de/gothaer/collections/Stapel.java
@@ -1,31 +1,34 @@
 package de.gothaer.collections;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Stapel {
 	
 	private static final int DEFAULT_SIZE = 10;
-	private Object[] data;
-	private int index;
+	private List<Object> data;
+	
 	
 	public Stapel() {
-		data = new Object[DEFAULT_SIZE];
+		data = new ArrayList<>();
 	}
 	
 	public void push(Object value) throws StapelException {
 		if(isFull()) throw new StapelException("Overflow");
-		data[index ++] = value;
+		data.add(value);
 	}
 
 	public Object pop() throws StapelException {
 		if(isEmpty()) throw new StapelException("Underflow");
-		return data[--index];
+		return data.remove(data.size()-1);
 	}
 	
 	public boolean isEmpty() {
-		return index <= 0;
+		return data.isEmpty();
 	}
 	
 	public boolean isFull() {
-		return index >= data.length;
+		return data.size() >= DEFAULT_SIZE;
 	}
 	
 
