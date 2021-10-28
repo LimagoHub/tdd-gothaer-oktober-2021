fcdf35529001a60855e97652b0e5aa58617a221e
Jo Wagner <jowagner@limago.de>
2021-10-28 15:18:29 +0200

    mit test

---
+++ euro2dollar/src/test/java/de/gothaer/euro2dollar/AppTest.java
@@ -0,0 +1,21 @@
+package de.gothaer.euro2dollar;
+
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
